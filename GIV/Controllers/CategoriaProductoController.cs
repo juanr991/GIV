82084c395c922700ccf6083196edc0bd6367597c
Juan Felipe Alzate Aguirre <Juan Felipe Alzate Aguirre>
2017-09-20 16:35:04 -0500

    Agregar archivos de proyecto.

---
+++ GIV/Controllers/CategoriaProductoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GIV.Models;
+
+namespace GIV.Controllers
+{
+    public class CategoriaProductoController : Controller
+    {
+        // GET: CategoriaProducto
+        public ActionResult CategoriaControler()
+        {
+            List<CategoriaProducto> lista = new List<CategoriaProducto>();
+
+            CategoriaProducto categoria1 = new CategoriaProducto();
+            categoria1.IdCategoria = new Guid();
+            categoria1.Nombre = "Renault";
+            categoria1.Descripcion = "Categoria Francesa";
+            lista.Add(categoria1);
+
+            CategoriaProducto categoria2 = new CategoriaProducto();
+            categoria2.IdCategoria = new Guid();
+            categoria2.Nombre = "Mazda";
+            categoria2.Descripcion = "Categoria Japonesa";
+            lista.Add(categoria2);
+
+            CategoriaProducto categoria3 = new CategoriaProducto();
+            categoria3.IdCategoria = new Guid();
+            categoria3.Nombre = "Chevrolet";
+            categoria3.Descripcion = "Categoria Estadounidense";
+            lista.Add(categoria3);
+
+
+
+
+            return View(lista);
+        }
+    }
+}
---
+++ GIV/Models/CategoriaProducto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GIV.Models
+{
+    public class CategoriaProducto
+    {
+        public Guid     IdCategoria     { get; set; }
+        public string   Nombre          { get; set; }
+        public string   Descripcion     { get; set; }
+
+    }
+}
